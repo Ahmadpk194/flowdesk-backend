b05c9d17bbabfc6c39cadee7e2ecb076b17647c8
Ahmad <ahmadpk194@gmail.com>
2026-05-08 22:48:46 +0500

    cors update

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   app.use(cookieParser());
 
